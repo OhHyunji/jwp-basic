d7184a5b6ff29b0557e8d70de593fbc5556610f1
azalea <azalea.oh@kakaocorp.com>
2020-05-16 03:18:02 +0900

    10.1.2.2 test로 시작하는 메소드 실행하기

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -2,10 +2,25 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
+    private static final String TEST_METHOD_PREFIX = "test";
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test junit3Test = clazz.newInstance();
 
+        Method[] methods = clazz.getMethods();
+        Arrays.stream(methods).filter(method -> method.getName().startsWith(TEST_METHOD_PREFIX)).forEach(m -> {
+            try {
+                m.invoke(junit3Test);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,6 @@
 package core.ref;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,13 +8,35 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 import next.model.User;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    private static final String SEPARATOR = ",";
 
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+
+        logger.debug("########## {} Info ##########", clazz.getName());
+
+        logger.debug("<Fields:*>");
+        logger.debug(StringUtils.join(toFieldNames(clazz.getDeclaredFields()), SEPARATOR));
+        logger.debug("<Fields:public>");
+        logger.debug(StringUtils.join(toFieldNames(clazz.getFields()), SEPARATOR));
+
+        logger.debug("<Constructors:*>");
+        Arrays.stream(clazz.getDeclaredConstructors()).forEach(c -> logger.debug(c.toString()));
+        logger.debug("<Constructors:public>");
+        Arrays.stream(clazz.getConstructors()).forEach(c -> logger.debug(c.toString()));
+
+        logger.debug("<Methods:*>");
+        Arrays.stream(clazz.getDeclaredMethods()).forEach(c -> logger.debug(c.toString()));
+        logger.debug("<Methods:public>");
+        Arrays.stream(clazz.getMethods()).forEach(c -> logger.debug(c.toString()));
     }
     
     @Test
@@ -26,5 +49,9 @@ public class ReflectionTest {
     public void privateFieldAccess() {
         Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
+    }
+
+    private List<String> toFieldNames(Field[] fields) {
+        return Arrays.stream(fields).map(Field::getName).collect(Collectors.toList());
     }
 }
