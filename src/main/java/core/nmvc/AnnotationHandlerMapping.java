0679f97a3d69d297e3adb40495ce008624dda34f
HyunjiOh <hyunji9212@gmail.com>
2020-05-16 18:38:08 +0900

    10.1.3.2 AnnotationHandlerMapping initialize 구현

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,14 +1,14 @@
 package core.nmvc;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.google.common.collect.Maps;
 
+import core.annotation.RequestMapping;
 import core.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
@@ -20,13 +20,53 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
+    /**
+     * step1. @Controller 붙어있는 클래스들을 찾아서
+     * step2. (url, HTTP method) 요청에 적합한 method를 호출하도록 Map<HandlerKey, HandlerExecution> handlerExecutions 을 채운다.
+     *  - HandlerKey: url, HTTP method
+     *  - HandlerExecution: 실행할 method
+     */
     public void initialize() {
         final Map<Class<?>, Object> controllers = ControllerScanner.scan();
+
+        controllers.entrySet().stream()
+                .flatMap(this::toRequestMappingElements)
+                .forEach(r -> handlerExecutions.put(r.createHandlerKey(), r.createHandlerExecution()));
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
         return handlerExecutions.get(new HandlerKey(requestUri, rm));
+    }
+
+    private Stream<_RequestMappingElement> toRequestMappingElements(Map.Entry<Class<?>, Object> entry) {
+
+        final Class<?> controller = entry.getKey();
+        final Object controllerInstance = entry.getValue();
+
+        return Arrays.stream(controller.getMethods())
+                .filter(m -> m.isAnnotationPresent(RequestMapping.class))
+                .map(m -> new _RequestMappingElement(controllerInstance, m, m.getAnnotation(RequestMapping.class)));
+    }
+
+    private class _RequestMappingElement {
+        private Object instance;
+        private Method method;
+        private RequestMapping requestMapping;
+
+        public _RequestMappingElement(Object instance, Method method, RequestMapping requestMapping) {
+            this.instance = instance;
+            this.method = method;
+            this.requestMapping = requestMapping;
+        }
+
+        public HandlerKey createHandlerKey() {
+            return new HandlerKey(requestMapping.value(), requestMapping.method());
+        }
+
+        public HandlerExecution createHandlerExecution() {
+            return new HandlerExecution(instance,  method);
+        }
     }
 }
