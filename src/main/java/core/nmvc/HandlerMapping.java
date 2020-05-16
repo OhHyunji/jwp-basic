f0036de738907fac4d2fcbc9e1ecf8a268f0bb05
HyunjiOh <hyunji9212@gmail.com>
2020-05-16 19:44:10 +0900

    10.2.4 HandlerMapping 인터페이스 선언, LegacyHandlerMapping&AnnotationHandlerMapping 구현, DispatcherServlet에 적용

--- src/main/java/core/nmvc/HandlerMapping.java
+++ src/main/java/core/nmvc/HandlerMapping.java
@@ -2,6 +2,10 @@ package core.nmvc;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * {@link core.mvc.DispatcherServlet} 에서
+ * {@link core.mvc.LegacyHandlerMapping}, {@link AnnotationHandlerMapping} 를 통합해서 지원하기 위한 인터페이스
+ */
 public interface HandlerMapping {
     void initialize();
     Object getHandler(HttpServletRequest request);
