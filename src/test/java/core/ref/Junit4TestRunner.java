016d16124ec40310fd08689fd774b41585c288b9
HyunjiOh <hyunji9212@gmail.com>
2020-05-16 16:31:54 +0900

    10.1.2.3 @MyTest 애노테이션으로 설정된 메소드 실행하기

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -2,10 +2,26 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
+
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.newInstance();
+
+        Arrays.stream(clazz.getMethods())
+                .filter(m -> Arrays.stream(m.getDeclaredAnnotations()).anyMatch(a ->a.annotationType().equals(MyTest.class)))
+                .forEach(m -> {
+                    try {
+                        m.invoke(junit4Test );
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
 
     }
 }
