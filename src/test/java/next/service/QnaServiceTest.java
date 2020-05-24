5f20d748f9b9aa1178868a9a2df63592671f52f8
HyunjiOh <hyunji9212@gmail.com>
2020-05-24 16:35:41 +0900

    11.2 QnaServiceTest 채우기

--- src/test/java/next/service/QnaServiceTest.java
+++ src/test/java/next/service/QnaServiceTest.java
@@ -1,7 +1,55 @@
 package next.service;
 
-import static org.junit.Assert.*;
+import next.CannotDeleteException;
+import next.dao.AnswerDao;
+import next.dao.QuestionDao;
+import next.model.Question;
+import next.model.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
 
 public class QnaServiceTest {
+    private QnaService mockQnaService;
+    private QuestionDao mockQuestionDao;
+    private AnswerDao mockAnswerDao;
 
+    @Before
+    public void setUp() {
+        mockQuestionDao = new MockQuestionDao();
+        mockAnswerDao = new MockAnswerDao();
+        mockQnaService = QnaService.getInstance(mockQuestionDao, mockAnswerDao);
+    }
+
+    @Test(expected = CannotDeleteException.class)
+    public void deleteQuestion_notExist_questionId() throws CannotDeleteException {
+        mockQnaService.deleteQuestion(1L, newUser("az"));
+    }
+
+    @Test(expected = CannotDeleteException.class)
+    public void deleteQuestion_anotherUser() throws CannotDeleteException {
+        long mockQuestionId = 1;
+        Question question = newQuestion(mockQuestionId, "az");
+        mockQuestionDao.insert(question);
+
+        mockQnaService.deleteQuestion(mockQuestionId, newUser("olaf"));
+    }
+
+    @Test
+    public void deleteQuestion_notExist_sameWriter() throws CannotDeleteException {
+        long mockQuestionId = 1;
+        Question question =  newQuestion(mockQuestionId, "az");
+        mockQuestionDao.insert(question);
+
+        mockQnaService.deleteQuestion(mockQuestionId, newUser("az"));
+    }
+
+    private Question newQuestion(long questionId, String userId) {
+        return new Question(questionId, userId, "title", "contents", new Date(), 0);
+    }
+
+    private User newUser(String userId) {
+        return new User(userId, "password", "name", "email");
+    }
 }
